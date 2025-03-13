2f5eb7affd895522106f3224226c639b73891522
buyath <buyaththayub@gmail.com>
2025-03-13 23:52:55 +0000

    quiz app done

--- quizApp/app.js
+++ quizApp/app.js
@@ -83,16 +83,42 @@ function selectAnswer(e){
     let  isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }
     else{
         selectedBtn.classList.add("incorrect");
     }
-    // Array.from(answerButtons.children).forEach(button => {
-    //     if(button.dataset.correct === "true"){
-    //         button.classList.add("correct");
-    //     }
-    //     button.disabled = true;
-    // })
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block"; 
 
 }
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `your score ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextbutton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+nextButton.addEventListener("click",() => {
+    if(currentQuestionIndex < questions.length){
+        handleNextbutton();
+    }
+    else{
+        startquiz();
+    }
+})
 startquiz();
