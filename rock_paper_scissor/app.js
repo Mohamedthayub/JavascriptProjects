30a548bee60bd3d5700abcc435a068c140dd4781
buyath <buyaththayub@gmail.com>
2025-03-18 09:01:57 +0000

    final code for rock paper scissor

--- rock_paper_scissor/app.js
+++ rock_paper_scissor/app.js
@@ -1,59 +1,61 @@
-const choices = document.querySelectorAll(".choice");
+let choices = document.querySelectorAll(".choice");
+let userScore = document.querySelector("#user-score");
+let computerScore = document.querySelector("#comp-score");
 let message = document.querySelector("#msg");
-let computerScore = document.getElementById("comp-score");
-let userScore = document.getElementById("user-score");
 
-let computerPoint = 0;
 let userPoint = 0;
-
-
-function genComputerChoice(){
-    let compChoice = ["rock","paper","scissor"];
-    let rand = Math.floor(Math.random() *3);
-    return compChoice[rand];
+let computerPoint = 0;
+function genComputer(){
+    let choice = ["rock","paper","scissor"];
+    let rand = Math.floor(Math.random() * 3);
+    return choice[rand];
 }
-// function  showWinner(winningMessage,userChoice,computerChoice){
-//     if(winningMessage){
-//         message.innerText =`winner is computer ${computerChoice}`;
-//     }
-//     else{
-//         message.innerText = `winner is user ${userChoice}`;
-//     }
+function showWinner(userWin,userchoice,computerChoice){
+if(userWin){
+    message.innerText = `user wins ${userchoice} beats ${computerChoice}`;
+    message.style.backgroundColor = "green";
+    userPoint++;
+    userScore.innerText = userPoint;
 
-// }
-function showWinner(winner){
-    if(winner){
-        console.log("winner is ",winner);
+}
+else{
+    message.innerText =  `computer wins ${computerChoice} beats ${userchoice}`;
+    message.style.backgroundColor = "red";
+    computerPoint++;
+    computerScore.innerText = computerPoint;
+
+}
+}
+function MatchDraw(){
+    message.innerText = `Match is Draw `;
+    message.style.backgroundColor = "yellow";
+
+}
+function checkWinner(userchoice){
+    let computerChoice = genComputer();
+    if(userchoice == computerChoice){
+        MatchDraw();
+
     }
     else{
-        console.log("winner is ",winner);
+        let userWin = true;
+        if(userchoice == "rock"){
+            userWin = computerChoice == "paper"  ? false : true;
+        }
+        else if(userchoice == "paper"){
+            userWin = computerChoice == "scissor" ? false : true;
+        }
+        else{
+            userWin = computerChoice == "rock" ? false : true;
+        }
+        showWinner(userWin,userchoice,computerChoice);
     }
    
 }
-function  checkWinner(userChoice){
-    let computerChoice = genComputerChoice();
-    if(userChoice == computerChoice){
-        alert("match draw");
-    }
-    else{
-        let winningMessage = true;
-        if(userChoice == "rock"){
-            winningMessage = computerChoice == "paper" ? false : true;
-        }
-        else if (userChoice == "paper"){
-            winningMessage = computerChoice == "scissor" ? false : true;
-        }
-        else{
-            winningMessage = computerChoice == "rock" ? false : true;
-        }
-        showWinner(winningMessage);
-    }
-    // showWinner(userChoice,computerChoice,winningMessage);
-}
-
 choices.forEach(box => {
     box.addEventListener("click",() => {
-        let userChoice = box.getAttribute("id");
-        checkWinner(userChoice);
+        let user = box.getAttribute("id");
+        checkWinner(user);
     })
 })
+
