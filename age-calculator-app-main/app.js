ac0e5051518f58bb6a81d61d5f1ad6285352bd30
buyath <buyaththayub@gmail.com>
2025-03-20 09:49:45 +0000

    age calculator done

---
+++ age-calculator-app-main/app.js
@@ -0,0 +1,46 @@
+const  userDayInput  = document.getElementById("day");
+const userMonthInput = document.getElementById("month");
+const userYearInput = document.getElementById("year");
+const button = document.getElementById("btn");
+const showYear = document.getElementById("showyear");
+const showMonth = document.getElementById("monthh1");
+const showDay = document.getElementById("showday");
+// console.log(date);
+button.addEventListener("click",function(e){
+    if(userDayInput.value == "" || userMonthInput.value == "" || userYearInput.value == ""){
+        alert("it should not  be empty");
+        e.preventDefault();
+    }
+    else{
+        calculateAge(userDayInput.value,userMonthInput.value,userYearInput.value);
+        userDayInput.value = "";
+        userMonthInput.value = "";
+        userYearInput.value = "";
+    }
+})
+function calculateAge(birthdate, birthmonth, birthyear) {
+    let dates = new Date();
+    let day = dates.getDate();
+    let month = dates.getMonth() + 1; 
+    let year = dates.getFullYear();
+
+    let userYear = year - birthyear;
+    let userMonth = month - birthmonth;
+    let userDate = day - birthdate;
+
+    if (userDate < 0) {
+        userMonth -= 1;  
+        let previousMonthDays = new Date(year, month - 1, 0).getDate(); 
+        userDate += previousMonthDays;
+    }
+
+    if (userMonth < 0) {
+        userYear -= 1;  
+        userMonth += 12;
+    }
+
+    showDay.innerText = userDate;
+    showMonth.innerText = userMonth;
+    showYear.innerText = userYear;
+}
+
