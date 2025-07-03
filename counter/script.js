4596972961300ce586f19afaaa4802272962af12
buyath <buyaththayub@gmail.com>
2025-07-03 20:22:14 +0000

    i have added clickCounts  feature in counterApp

--- counter/script.js
+++ counter/script.js
@@ -3,25 +3,43 @@ const increaseBtn = document.querySelector("#increase");
 const decreaseBtn = document.querySelector("#decrease");
 const resetButton = document.querySelector("#reset");
 const clickCountsResult = document.querySelector("#clickcount");
+const countHistory = document.querySelector(".count-history");
 let count = 0; 
-
 let countClick = 0;
+let clickCounts = [];
+
+function addClickCount(count){
+    if(clickCounts.length >  10){
+        clickCounts.shift();
+    }
+    else{
+        clickCounts.push(count);
+        clickCounts.forEach((item) => {
+            const para = document.createElement("p");
+            para.innerText = item;
+            countHistory.appendChild(para);
+        })
+    }
+}
 // let countClicks = [];
 increaseBtn.addEventListener("click",() => {
     count += 1;
     counterResult.innerText = `Count is : ${count}`
     countClick++
     clickCountsResult.innerText = `click counts : ${countClick}`
+    addClickCount(countClick);
 })
 decreaseBtn.addEventListener("click",() => {
     count -= 1
     counterResult.innerText = `Count is : ${count}`
     countClick++;
-    clickCountsResult.innerText =  `click counts : ${countClick}`
+    clickCountsResult.innerText =  `click counts : ${countClick}`;
+    addClickCount(countClick);
 })
 resetButton.addEventListener("click",() =>{
     count = 0;
     counterResult.innerText = `Count is : ${count}`;
     countClick++;
     clickCountsResult.innerText  =  `click counts : ${countClick}`
+     addClickCount(countClick);
 });
