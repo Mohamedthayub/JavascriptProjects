c4cacdc5ac3c73e56c3bec522f1c41aaed5327ab
buyath <buyaththayub@gmail.com>
2025-07-04 12:29:38 +0000

    localstorge exercise added

---
+++ localstorage/app.js
@@ -0,0 +1,39 @@
+
+        const addData = document.querySelector("#add-data");
+        const deleteData = document.querySelector("#dlt-data");
+        const showData = document.querySelector("#show-data");
+        const dataContainer = document.querySelector(".data-container");
+
+        let name = ["mohamed","mohidee","thayub","nadheem"];
+
+        function createItem(data){
+            let personName  = document.createElement("p");
+            personName.innerText = data;
+            dataContainer.appendChild(personName);
+
+        }
+        addData.addEventListener("click",() => {
+            localStorage.setItem("item",JSON.stringify(name));
+        })
+
+        showData.addEventListener("click",() => {
+            let newData = localStorage.getItem("item");
+            name = newData ? JSON.parse(newData) : [];
+            name.forEach((item) => {
+                createItem(item)
+            })
+            console.log("it is working well ... ")
+        })
+        deleteData.addEventListener("click",() => {
+            localStorage.removeItem("item");
+        })
+        window.onload = () => {
+            try{
+                newData.forEach((data) => {
+                    createItem(data);
+                })
+            }
+            catch(err){
+                console.log(err.message);
+            }
+        }
